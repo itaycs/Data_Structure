f0f02fbd42fd9cfbaf9ddcb20003ca7a87b6a09e
ItayY <itay.yeshaya@campus.technion.ac.il>
2019-05-21 00:39:02 +0300

    Initial commit

---
+++ LinkedList.h
@@ -0,0 +1,17 @@
+//
+// Created by ItayY on 21/05/2019.
+// General implement of linked list using template
+//
+
+#ifndef DATA_STRUCTURE_LINKEDLIST_H
+#define DATA_STRUCTURE_LINKEDLIST_H
+
+template <class T>
+class Node{
+private:
+    T Data;
+    Node<T> *next;
+public:
+    
+};
+#endif //DATA_STRUCTURE_LINKEDLIST_H
